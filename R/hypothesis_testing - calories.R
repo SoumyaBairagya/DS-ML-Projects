5968eedb30f21dd55d0369e45768b9b33caec565
Soumya Bairagya <78803599+SoumyaBairagya@users.noreply.github.com>
2023-01-24 22:51:51 +0530

    Update hypothesis_testing - calories.R

--- R/hypothesis_testing - calories.R
+++ R/hypothesis_testing - calories.R
@@ -1,4 +1,3 @@
-#ABHA MARATHE1:38 PM
 #The nutrition label on a bag of potato chips says that a one ounce
 #(28 gram) serving of potato chips has 130 calories and contains ten grams of fat, with three grams
 #of saturated fat. A random sample of 35 bags yielded a sample mean of 134 calories with a standard
